3781c7522965d05beb6594c1ccf639200f4b22bf
Prabin Shrestha <whopravinewa@gmail.com>
2021-10-04 17:35:48 +0545

    Server chat fixed

--- Chat.js
+++ Chat.js
@@ -1,35 +1,35 @@
-const socket = io('http://localhost:3000')
+const socketChat = io("http://localhost:3333");
 // console.log('i am chat app')
-const messageContainer = document.getElementById('message-container')
+const messageContainer = document.getElementById("message-container");
 
-socket.on('chat-message', message => {
-    console.log(message)
-    appendMessageRight('Chit : ' + message)
-})
+socketChat.on("chat-message", (message) => {
+  console.log(message);
+  appendMessageRight("Chit : " + message);
+});
 
-const chatMessageForm = document.getElementById('send-container')
+const chatMessageForm = document.getElementById("send-container");
 // const chatmessageContainer = document.getElementById('message-container')
-let chatMessage = document.getElementById('message-input')
+let chatMessage = document.getElementById("message-input");
 
-chatMessageForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const message = chatMessage.value
-    console.log(message)
-    appendMessageLeft('You : ' + message)
-    socket.emit('chat-message', message)
-    chatMessage.value = ''
-})
+chatMessageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = chatMessage.value;
+  console.log(message);
+  appendMessageLeft("You created message is : " + message);
+  socketChat.emit("chat-message", message);
+  chatMessage.value = "";
+});
 
 function appendMessageRight(message) {
-    const messageElement = document.createElement('div')
-    messageElement.innerText = message
-    messageElement.align = 'right'
-    messageContainer.append(messageElement)
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageElement.align = "right";
+  messageContainer.append(messageElement);
 }
 
 function appendMessageLeft(message) {
-    const messageElement = document.createElement('div')
-    messageElement.innerText = message
-    messageElement.align = 'left'
-    messageContainer.append(messageElement)
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageElement.align = "left";
+  messageContainer.append(messageElement);
 }
--- Chit.js
+++ Chit.js
@@ -1,35 +1,34 @@
 // let us suppose chit wants to talk to chat(lets suppose another person)
-const socket = io('http://localhost:3000')
+const socketChat = io("http://localhost:3000");
 
-const chitMessageForm = document.getElementById('send-container')
-const chitMessageContainer = document.getElementById('message-container')
-let chitMessage = document.getElementById('message-input')
+const chitMessageForm = document.getElementById("send-container");
+const chitMessageContainer = document.getElementById("message-container");
+let chitMessage = document.getElementById("message-input");
 
-chitMessageForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const message = chitMessage.value
-    console.log(message)
-    appendMessageLeft('You : ' + message)
-    socket.emit('chit-message', message)
-    chitMessage.value = ''
-})
+chitMessageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = chitMessage.value;
+  console.log(message);
+  appendMessageLeft("You : " + message);
+  socketChat.emit("chit-message", message);
+  chitMessage.value = "";
+});
 
 function appendMessageRight(message) {
-    const messageElement = document.createElement('div')
-    messageElement.innerText = message
-    messageElement.align = 'right'
-    chitMessageContainer.append(messageElement)
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageElement.align = "right";
+  chitMessageContainer.append(messageElement);
 }
 
 function appendMessageLeft(message) {
-    const messageElement = document.createElement('div')
-    messageElement.innerText = message
-    messageElement.align = 'left'
-    chitMessageContainer.append(messageElement)
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageElement.align = "left";
+  chitMessageContainer.append(messageElement);
 }
 
-
-socket.on('chit-message', message => {
-    console.log(message)
-    appendMessageRight('Chat : ' + message)
-})
+socketChat.on("chit-message", (message) => {
+  console.log(message);
+  appendMessageRight("Chat : " + message);
+});
